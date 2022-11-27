bca952ffbf303bb6b0115ef50b5c4bb6f91321af
M1shan9 <118421201+M1shan9@users.noreply.github.com>
2022-11-27 21:04:51 +0900

    Add files via upload

---
+++ Классы/Классы/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Классы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+   Users user = new Users()
+   {
+    Name = "Mihail",
+    Surname = "Aslamov",
+    Login = "admin",
+    Password = "admin",
+   };
+   Console.WriteLine(user.Name);
+   Console.WriteLine("Write name");
+   user.Name = Console.ReadLine();
+   Console.WriteLine($"Write new name - {user.Name}");
+   Console.ReadLine();
+  }
+
+  class Users
+  {
+   public string Name;
+   public string Surname;
+
+   public string Login;
+   public string Password;
+  }
+ }
+    }
+
